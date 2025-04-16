b48ad3aba6486d154b85cfc4adcd8e8d689b63e9
mena <mena.rizkalla1@gmail.com>
2025-04-16 17:54:37 +0200

    ensure no repetition in suggestions

--- src/main/kotlin/logic/SuggestEggFreeSweetUseCase.kt
+++ src/main/kotlin/logic/SuggestEggFreeSweetUseCase.kt
@@ -11,17 +11,19 @@ class SuggestEggFreeSweetUseCase(
 
     fun suggestEggFreeSweet() : Meal? {
         val allMeals = mealsRepository.getAllMeals()
-        val eggFreeSweets = allMeals.filter { meal ->
-            meal.tags?.any { tag -> tag.contains("sweet", ignoreCase = true)
-                    && meal.ingredients?.none { ingredient -> ingredient.equals("egg", ignoreCase = true) } == true
-                    && !suggestedSweets.contains(meal.name)
-            } == true
-        }
+        val eggFreeSweets = allMeals.filter(::filterEggFreeSweets)
 
         if (eggFreeSweets.isEmpty()) return null
 
         val randomSweet = eggFreeSweets.random(Random)
+        suggestedSweets.contains(randomSweet.name)
         return randomSweet
     }
 
+    private fun filterEggFreeSweets(meal: Meal): Boolean {
+        return meal.tags.any { tag -> tag.contains("sweet", ignoreCase = true)
+                && meal.ingredients.none { ingredient -> ingredient.equals("egg", ignoreCase = true) }
+                && !suggestedSweets.contains(meal.name)
+        }
+    }
 }
