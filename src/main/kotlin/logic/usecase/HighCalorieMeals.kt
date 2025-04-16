fbc5c0d0ce5bac8a9e5fb169be338a9a31573cfe
Diyar <diyarhussein727@gmail.com>
2025-04-16 19:36:45 +0300

    add high calorie meals usecase

---
+++ src/main/kotlin/logic/usecase/HighCalorieMeals.kt
@@ -0,0 +1,24 @@
+package org.berlin.logic.usecase
+
+import org.berlin.logic.MealsRepository
+import org.berlin.model.Meal
+import kotlin.random.Random
+
+class HighCalorieMeals (private val mealsRepository: MealsRepository){
+    private fun getHighCalorieMeals(calorieThreshold: Double = 700.0): List<Meal> {
+        return mealsRepository.getAllMeals()
+            .filter { meal -> meal.nutrition.calories > calorieThreshold }
+            .sortedByDescending { it.nutrition.calories }
+    }
+
+    fun suggestHighCalorieMeal(calorieThreshold: Double = 700.0): Result<Meal> {
+        val highCalorieMeals = getHighCalorieMeals(calorieThreshold = calorieThreshold)
+
+        return if (highCalorieMeals.isEmpty()) {
+            Result.failure(NoSuchElementException("No meals found with more than $calorieThreshold calories"))
+        }else {
+            val randomIndex = Random.nextInt(highCalorieMeals.size)
+            Result.success(highCalorieMeals[randomIndex])
+        }
+    }
+}
