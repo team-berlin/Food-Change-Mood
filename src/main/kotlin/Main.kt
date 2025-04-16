1b442f6967af4165e27a36099ae6dca38a79e107
Fatma Hegazy <fatma@email.com>
2025-04-16 18:19:10 +0200

    add easy food suggestion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.berlin
 import dependencyInjection.appModule
 import org.berlin.dependencyInjection.easyFoodSuggestionModule
 import org.berlin.logic.usecase.EasyFoodSuggestionRepository
+import org.berlin.presentation.FoodChangeMoodUI
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 
@@ -11,9 +12,12 @@ fun main() {
     startKoin{
         modules(appModule, easyFoodSuggestionModule)
     }
+
     val easyFoodSuggestionRepository: EasyFoodSuggestionRepository = getKoin().get()
     easyFoodSuggestionRepository.getEasyFoodSuggestion().also {
         println(it)
     }
+    val ui: FoodChangeMoodUI = getKoin().get()
+    ui.start()
 }
 
