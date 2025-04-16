0fab0a44baa0848ec025a00d39020c4acf66ef90
Diyar <diyarhussein727@gmail.com>
2025-04-16 16:28:10 +0300

    inject quick healthy meals

--- src/main/kotlin/dependency_injection/appModule.kt
+++ src/main/kotlin/dependency_injection/appModule.kt
@@ -4,13 +4,17 @@ import com.berlin.data.CsvFileReader
 import com.berlin.data.CsvMealsRepository
 import com.berlin.data.MealsCsvParser
 import org.berlin.logic.MealsRepository
+import org.berlin.logic.usecase.QuickHealthyMeals
 import org.koin.dsl.module
 import java.io.File
 
 val appModule = module {
+        // Data Layer
         single { File("food.csv") }
         single { CsvFileReader(get()) }
         single { MealsCsvParser() }
-
         single<MealsRepository> { CsvMealsRepository(get(), get()) }
+
+        // Logic Layer
+        single { QuickHealthyMeals(get()) }
 }
