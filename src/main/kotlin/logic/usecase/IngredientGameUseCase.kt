45073462d90338c609f0f84ce1123058f51d6441
Marwan Qashwa <marawankashwa24@gmail.com>
2025-04-18 10:24:28 +0200

    make usecase faster

--- src/main/kotlin/logic/usecase/IngredientGameUseCase.kt
+++ src/main/kotlin/logic/usecase/IngredientGameUseCase.kt
@@ -4,22 +4,18 @@ import org.berlin.logic.MealsRepository
 import org.berlin.model.MealForIngredientGame
 
 class IngredientGameUseCase(
-    private val repository: MealsRepository,
-    private val ingredientGameMealsMapper: IngredientGameMealsMapper
+    repository: MealsRepository,
+    ingredientGameMealsMapper: IngredientGameMealsMapper
 ) {
 
+    private val meals=repository.getAllMeals()
+    private val mealsForGame=ingredientGameMealsMapper.map(meals)
     fun getFifteenMeals(): List<MealForIngredientGame> {
-
-        val meals=repository.getAllMeals()
-        if (meals.isEmpty()||meals.size< MAX_QUESTIONS)throw Exception("need more meals list from repo")
-
-        return ingredientGameMealsMapper.map(meals)
+        if (mealsForGame.isEmpty()||mealsForGame.size< MAX_QUESTIONS)throw Exception("need more meals to run a game")
+        return mealsForGame
             .shuffled()
             .take(MAX_QUESTIONS)
-
-
     }
-
     private companion object Constants {
         const val MAX_QUESTIONS = 15
 
