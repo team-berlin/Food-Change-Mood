98400c568869597ae3a271f0bf754cfc057dd416
mena <mena.rizkalla1@gmail.com>
2025-04-21 23:26:39 +0200

    add suggestEggFreeSweetUI

---
+++ src/main/kotlin/presentation/suggest/SuggestEggFreeSweetUI.kt
@@ -0,0 +1,51 @@
+package org.berlin.presentation.suggest
+
+import org.berlin.logic.usecase.suggest.SuggestEggFreeSweetUseCase
+import org.berlin.model.Meal
+import org.berlin.presentation.UiRunner
+import org.berlin.presentation.input_output.Reader
+import org.berlin.presentation.input_output.Viewer
+
+class SuggestEggFreeSweetUI(
+    private val suggestEggFreeSweetUseCase: SuggestEggFreeSweetUseCase,
+    private val viewer: Viewer,
+    private val reader: Reader
+): UiRunner {
+    override val id: Int = 6
+    override val label: String = "Suggest Egg FreeSweet"
+
+    override fun run() {
+        val suggestion = suggestEggFreeSweetUseCase.suggestEggFreeSweet()
+        if (suggestion != null) {
+            viewer.display("\n--- Suggested Free Sweet ---")
+            viewer.display("Name: ${suggestion.name}")
+            viewer.display("Description: ${suggestion.description}")
+            viewer.display("---------------------------")
+            viewer.display("Like it? (yes/no/exit)")
+            when (reader.getUserInput()?.lowercase()) {
+                "yes" -> showSweetDetails(suggestion)
+                "no" -> {
+                    viewer.display("Disliked. Getting another suggestion.")
+                    run()
+                }
+
+                "exit" -> return // presentFeatures()
+                else -> viewer.display("Invalid Input")
+            }
+        } else {
+            println("No more egg-free sweets to suggest :( ")
+        }
+    }
+    private fun showSweetDetails(meal: Meal) {
+        viewer.display("\n--- Sweet Details ---")
+        viewer.display("Name: ${meal.name}")
+        viewer.display("Description: ${meal.description}")
+        viewer.display("Ingredients: ${meal.ingredients.joinToString(", ")}")
+        meal.steps.let {
+            viewer.display("Steps:")
+            it.forEachIndexed { index, step ->
+                viewer.display("${index + 1}. $step")
+            }
+        }
+    }
+}
