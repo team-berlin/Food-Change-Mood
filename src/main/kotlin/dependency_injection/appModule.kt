2bbf6488a694d07ce78da93f534ce586e943ce0a
Diyar <diyarhussein727@gmail.com>
2025-04-21 23:17:28 +0300

    fix file path after merge

--- src/main/kotlin/dependency_injection/appModule.kt
+++ src/main/kotlin/dependency_injection/appModule.kt
@@ -18,7 +18,7 @@ import org.koin.dsl.module
 import java.io.File
 
 val appModule = module {
-    single { File("D:\\TheChance\\food.csv") }
+    single { File("food.csv") }
     single { CsvFileReader(get()) }
     single { MealsCsvParser() }
     single<MealsRepository> { CsvMealsRepository(get(), get()) }
